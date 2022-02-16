41a1dfdd0b5f8cc66563fe4091983a069f828991
hyeonkimmm <43769123+hyeonkimmm@users.noreply.github.com>
2022-02-16 15:00:57 +0900

    update requirement & coding convention(flake8)

--- extract/shopping_best.py
+++ extract/shopping_best.py
@@ -7,24 +7,24 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import os
+# import os
 
-## Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
+# Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
 driver = webdriver.Chrome('C:\\Users\\hyeon\\Desktop\\side-project\\gift-data-backend-repo\\driver\\chromedriver')
 driver.implicitly_wait(3)
 driver.get('https://datalab.naver.com/shoppingInsight/sCategory.naver')
-## HTTP GET Request
+# HTTP GET Request
 req = requests.get('https://datalab.naver.com/shoppingInsight/sCategory.naver')
-#content > div.section_instie_area.space_top > div > div.section.insite_inquiry > div > div > div:nth-child(1) > div > div:nth-child(1) > span
+# content > div.section_instie_area.space_top > div > div.section.insite_inquiry > div > div > div:nth-child(1) > div > div:nth-child(1) > span
 driver.find_element_by_xpath('//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/span').click()
 driver.find_element_by_xpath('//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/ul/li[5]/a').click()
 
-## 순위 결과 가져오기
+# 순위 결과 가져오기
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 a = soup.select('#content > div.section_instie_area.space_top > div > div:nth-child(2) > div.section_insite_sub > div > div > div.rank_top1000_scroll > ul')
 extracted = a[0].text
-extracted = extracted.replace(' ','')
+extracted = extracted.replace(' ', '')
 f = open("새파일.txt", 'w')
 f.write(extracted)
 f.close()
