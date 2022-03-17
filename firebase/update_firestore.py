0c659bfa301c2146854893799abfbc4902a93baf
hyeonkimmm <43769123+hyeonkimmm@users.noreply.github.com>
2022-03-17 17:58:38 +0900

    #5 해당 구조 #6 변경 부분에 맞게 DB 설계 구현
    productInfo 데이터도 추후 업데이트 예정

---
+++ firebase/update_firestore.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import glob
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+# # 경덕
+# cred = credentials.Certificate('../data/info/atte2gd-eb088-firebase-adminsdk-bftoy-36320b017e.json')
+# cred = credentials.Certificate('data/info/atte2gd-eb088-firebase-adminsdk-bftoy-36320b017e.json')
+
+# # 재희
+# cred = credentials.Certificate('../data/info/atte2jh-firebase-adminsdk-27fq5-24afbf0bb4.json')
+# cred = credentials.Certificate('data/info/atte2jh-firebase-adminsdk-27fq5-24afbf0bb4.json')
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        print(path.dirname(path.dirname(path.abspath(__file__))))
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+        from extract.helper import get_date
+    else:
+        from ..extract.helper import get_date
+    # cred_list = ['data/info/atte2gd-eb088-firebase-adminsdk-bftoy-36320b017e.json', 'data/info/atte2jh-firebase-adminsdk-27fq5-24afbf0bb4.json']
+    cred_list = ['data/info/atte2jh-firebase-adminsdk-27fq5-24afbf0bb4.json']
+    for cred_path in cred_list:
+        cred = credentials.Certificate(f'{cred_path}')
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
+        all_dir = glob.glob('data/crawling/*')
+        date = get_date(day='today')
+        for dir_path in all_dir:
+            info = dir_path.split('\\')[-1]
+            age, gender, cat_id, cat_name = info.split('_')
+            app_info_path = dir_path + '/today_best.csv'
+            product_path = dir_path + '/today_best_prodct.csv'
+            app_info = pd.read_csv(app_info_path, index_col=0)
+            product_info = pd.read_csv(product_path, index_col=0).fillna('')
+            app_info = app_info.to_dict(orient='records')
+            for i, app_data in enumerate(app_info):
+                db.collection(f'{gender}').document(f'{age}').collection(f'{cat_id}').document(f'{date}').\
+                    collection(f'{i+1}').document('appInfo').set(app_data)
+            # TODO product_info 추가
--- update_result.py
+++ update_result.py
@@ -7,15 +7,15 @@ import glob
 from update_score import get_top5_rank
 from pandas import json_normalize
 
-def get_api_result(search_word, start_num=1):
-    '''
-    '''
+def get_api_result(search_word='원피스', start_num=1):
     PATH = 'data/info/naver_api_info.json'
     with open(PATH, "r") as json_file:
         user_info = json.load(json_file)
         client_id = user_info['client_id']
         client_secret = user_info['client_secret']
     url = f'https://openapi.naver.com/v1/search/shop.json?query={search_word}&display=100&start={str(start_num)}'
+    # 검색용 url
+    web_url = f'https://search.shopping.naver.com/search/all?query={search_word}&cat_id=&frm=NVSHATC'
     headers = {"X-Naver-Client-Id": client_id, "X-Naver-Client-Secret": client_secret}
     res = requests.get(url, headers=headers)
     j = json.loads(res.text)
@@ -25,44 +25,51 @@ def get_api_result(search_word, start_num=1):
     # link 수정
     df['link'] = 'https://search.shopping.naver.com/catalog/' + df['productId']
     # lprice 타입 변경 및 평균, 표준편차를 통해 최저, 최고 가격 데이터범위 구하기.
-    df['lprice'] = df['lprice'].astype('int')
+    df['lprice'] = df['lprice'].astype('int').fillna(0)
     # TODO: ValueError: cannot convert float NaN to integer 에러 해결 (NaN 처리 해주기)
-    df['mean_price'] = int(df['lprice'].mean())
-    df['std_price'] = int(df['lprice'].std())
-    # l_price, h_price - 음수 방지
-    df['l_price'] = df[['mean_price', 'std_price']].apply(lambda x: (x[0] - x[1]) if (x[0] - x[1] > 0) else x[0], axis=1)
-    df['h_price'] = df['mean_price'] + df['std_price']
+    # TODO: 검색 결과가 없거나 적은 경우
+    df['meanPrice'] = int(df['lprice'].mean())
+    df['stdPrice'] = int(df['lprice'].std())
+    # lowPrice - 음수 방지
+    df['lowPrice'] = df[['meanPrice', 'stdPrice']].apply(lambda x: (x[0] - x[1]) if (x[0] - x[1] > 0) else x[0], axis=1)
+    df['highPrice'] = df['meanPrice'] + df['stdPrice']
     # top 5 데이터만
     df = df.iloc[:5, :]
     df.reset_index(inplace=True)
     df['index'] = df['index'].apply(lambda x: x + 1)
     # 컬럼 정리
-    df = df[["index", "title", "link", "image", "lprice", "mean_price", "std_price", "l_price", "h_price", "productId", "brand", "maker", "category1", "category2", "category3", "category4"]]
-    df.rename(columns={'index': 'prduct_rank', 'lprice': 'price', 'productId': 'product_id'}, inplace=True)
+    df = df[["index", "title", "link", "image", "lprice", "meanPrice", "stdPrice", "lowPrice", "highPrice", "productId", "brand", "maker", "category1", "category2", "category3", "category4"]]
+    df.rename(columns={'index': 'productRank', 'lprice': 'productPrice'}, inplace=True)
     df['keyword'] = search_word
-    price_df = df[["keyword", "l_price", "h_price"]].iloc[0, :]
-    df = df[["prduct_rank", "title", "link", "image", "price", "product_id", "brand", "maker", "category1", "category2", "category3", "category4"]]
-    return df, price_df
+    appInfo = df[["keyword", "lowPrice", "highPrice"]].iloc[0, :]
+    appInfo['webUrl'] = web_url
+    appInfo['imageUrl'] = df.loc[0, 'image']
+    df = df[["productRank", "title", "link", "image", "productPrice", "productId", "brand", "maker", "category1", "category2", "category3", "category4"]]
+    return df, appInfo
 
 def update_today_best(path):
     score_path = path + '/score.csv'
     result = get_top5_rank(score_path)
+    result = result[['rank', 'keyword', 'score']]
     merge_df = pd.DataFrame()
     for i in range(5):
-        key = result.loc[i, 'keyword']
-        # 값 불러오기
-        df, price_df = get_api_result(key)
-        df_dict = df.to_dict(orient='records')
+        try:
+            key = result.loc[i, 'keyword']
+            # 값 불러오기
+            df, appInfo = get_api_result(key)
+        except Exception:
+            print(f'error {key}, {path}')
+            raise ValueError
+        # df_dict = df.to_dict(orient='records')
         # df_json = df.to_json(orient='records', force_ascii=False)
-        price_df['product_info'] = df_dict
-        merge_df = pd.concat([merge_df, price_df], axis=1)
+        # appInfo['product_info'] = df_dict
+        merge_df = pd.concat([merge_df, appInfo], axis=1)
     merge_df = merge_df.transpose()
+    merge_df.reset_index(drop=True, inplace=True)
     result = pd.merge(result, merge_df, how='left', on='keyword')
-    # result['rank'] = result['rank'].astype(object)
+    result.rename(columns={'keyword': 'keyWord'}, inplace=True)
     result.to_csv(path + '/today_best.csv')
-    # a = result.to_dict(orient='records')
-    # final = dict()
-    # final['data'] = a
+    df.to_csv(path + '/today_best_product.csv')
 
 
 # result의 keyword를 naver 쇼핑 api를 통해 검색하고 그 결과값을 계속 keyword, product_info 로 정리한 후 더함
@@ -70,3 +77,4 @@ if __name__ == '__main__':
     all_dir = glob.glob('data/crawling/*')
     for path in all_dir:
         update_today_best(path)
+        print(f'updated successfully {path}')
