a7925acce1d4d567c23b2d8791815f180baad3ea
hyeonkimmm <43769123+hyeonkimmm@users.noreply.github.com>
2022-03-30 19:49:04 +0900

    #8 중복되는 코드 refactoring

--- update_score.py
+++ update_score.py
@@ -34,6 +34,15 @@ def set_update_date(path, update_date):
     f.write(update_date)
     f.close()
 
+def apply_score(path, merge_df):
+    data = pd.read_csv(path)
+    data['score_'] = data['rank'].apply(lambda x: 21 - x)
+    data = data[['keyword', 'score_']]
+    merge_df = pd.merge(merge_df, data, how='outer', on='keyword').fillna(0)
+    merge_df['score'] = merge_df[['score', 'score_']].apply(lambda x: x[0] + x[1], axis=1)
+    merge_df = merge_df[['keyword', 'score']].sort_values(by='score', ascending=False)
+    return merge_df
+
 def calculation_score(directory):
     info = directory.split('\\')[-1]
     # age, gender, c_id, c_name = info.split('_')
@@ -43,37 +52,19 @@ def calculation_score(directory):
         if is_file(_path=directory, file_name='score.csv'):
             # 스코어 값이 있기 때문에 가장 최근 것만 추가
             merge_df = pd.read_csv(score[0])
-            update_date = file_list[-1].split('\\')[-1].split('_')[0]
+            extract_update_date = file_list[-1].split('\\')[-1].split('_')[0]
             # 파일이 어제 일자가 아니면 적용 X
             # 최종 업데이트 날짜 개별적용
             if is_file(_path=directory, file_name='update_date.txt'):
                 # update 파일이 있을 때
                 last_update_date = get_update_date(directory)
                 # 멱등성 만족
-                if last_update_date < update_date:
-                    # 최종 업데이트 날짜와 현재 업데이트할 날짜 비교
-                    # 업데이트
-                    data = pd.read_csv(file_list[-1])
-                    data['score_'] = data['rank'].apply(lambda x: 21 - x)
-                    data = data[['keyword', 'score_']]
-                    merge_df = pd.merge(merge_df, data, how='outer', on='keyword').fillna(0)
-                    merge_df['score'] = merge_df[['score', 'score_']].apply(lambda x: x[0] + x[1], axis=1)
-                    merge_df = merge_df[['keyword', 'score']].sort_values(by='score', ascending=False)
-                    merge_df.to_csv(path_or_buf=f'{directory}/score.csv', index=False)
-                    set_update_date(directory, update_date)
-                else:
+                if last_update_date > extract_update_date:
+                    # 최종 업데이트 날짜와 파일 상의 날짜 비교
+                    # 이미 업데이트 되었으므로 업데이트 하지 않음
                     print(f'already updated {info}')
-                    return
-            else:
-                # update 파일이 없을 때
-                data = pd.read_csv(file_list[-1])
-                data['score_'] = data['rank'].apply(lambda x: 21 - x)
-                data = data[['keyword', 'score_']]
-                merge_df = pd.merge(merge_df, data, how='outer', on='keyword').fillna(0)
-                merge_df['score'] = merge_df[['score', 'score_']].apply(lambda x: x[0] + x[1], axis=1)
-                merge_df = merge_df[['keyword', 'score']].sort_values(by='score', ascending=False)
-                merge_df.to_csv(path_or_buf=f'{directory}/score.csv', index=False)
-                set_update_date(directory, update_date)
+                else:
+                    merge_df = apply_score(path=file_list[-1], merge_df=merge_df)
         else:
             # 스코어 값이 없기 때문에 모든 file 추가
             file_path = file_list[0]
@@ -81,18 +72,13 @@ def calculation_score(directory):
             data['score'] = data['rank'].apply(lambda x: 21 - x)
             merge_df = data[['keyword', 'score']]
             for file_path in file_list[1:]:
-                data = pd.read_csv(file_path)
-                data['score_'] = data['rank'].apply(lambda x: 21 - x)
-                data = data[['keyword', 'score_']]
-                merge_df = pd.merge(merge_df, data, how='outer', on='keyword').fillna(0)
-                merge_df['score'] = merge_df[['score', 'score_']].apply(lambda x: x[0] + x[1], axis=1)
-                merge_df = merge_df[['keyword', 'score']].sort_values(by='score', ascending=False)
-                merge_df.to_csv(path_or_buf=f'{directory}/score.csv', index=False)
-            update_date = file_list[-1].split('\\')[-1].split('_')[0]
-            set_update_date(directory, update_date)
+                merge_df = apply_score(path=file_path, merge_df=merge_df)
+            extract_update_date = file_list[-1].split('\\')[-1].split('_')[0]
     except Exception:
         print(f'error occurred! {info}')
     else:
+        merge_df.to_csv(path_or_buf=f'{directory}/score.csv', index=False)
+        set_update_date(directory, extract_update_date)
         print(f'finish calculated to {info}')
 
 if __name__ == '__main__':
@@ -102,15 +88,3 @@ if __name__ == '__main__':
     for directory in all_dir:
         calculation_score(directory)
         # delete_file(directory)
-
-'''
- result = pd.read_csv(file_list[-1])
-# get top5
-result = result.iloc[:5, :].copy()
-result.loc[:, 'age'] = age
-result.loc[:, 'gender'] = gender
-result.loc[:, 'c_id'] = c_id
-result.loc[:, 'c_name'] = c_name
-result.loc[:, 'date'] = date
-result = result[['date', 'rank', 'age', 'gender', 'c_id', 'c_name', 'keyword']]
-'''
