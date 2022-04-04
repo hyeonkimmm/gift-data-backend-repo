97d65d52f1978a4437e7a0be7f2e36a7bccfc1e4
hyeonkimmm <43769123+hyeonkimmm@users.noreply.github.com>
2022-04-04 19:00:55 +0900

    #6 업데이트 코드 변경
    연령, 성별, 카테고리 적용

--- firebase/update_firestore.py
+++ firebase/update_firestore.py
@@ -34,11 +34,18 @@ if __name__ == '__main__':
             info = dir_path.split('\\')[-1]
             age, gender, cat_id, cat_name = info.split('_')
             app_info_path = dir_path + '/today_best.csv'
-            product_path = dir_path + '/today_best_prodct.csv'
-            app_info = pd.read_csv(app_info_path, index_col=0)
-            product_info = pd.read_csv(product_path, index_col=0).fillna('')
-            app_info = app_info.to_dict(orient='records')
+            # product_path = dir_path + '/today_best_prodct.csv'
+            app_info_df = pd.read_csv(app_info_path, index_col=0)
+            # product_info = pd.read_csv(product_path, index_col=0).fillna('')
+            app_info = app_info_df[['rank', 'keyword', 'score', 'lowPrice', 'highPrice', 'meanPrice', 'webUrl', 'imageUrl']].to_dict(orient='records')
+            # all_info = app_info_df['productInfo'].to_dict()
+            # app_info_df[['productInfo']]
             for i, app_data in enumerate(app_info):
                 db.collection(f'{gender}').document(f'{age}').collection(f'{cat_id}').document(f'{date}').\
                     collection(f'{i+1}').document('appInfo').set(app_data)
-            # TODO product_info 추가
+                print(f'update successfully {gender}_{age}_{cat_name}_{date}_{i+1}')
+            # # TODO product_info 추가
+            # for index, row in app_info_df.iterrows():
+            #     a = row['productInfo'].to_dict()
+            #     db.collection(f'{gender}').document(f'{age}').collection(f'{cat_id}').document(f'{date}').\
+            #         collection(f'{index+1}').document('appInfo').set(app_data)
